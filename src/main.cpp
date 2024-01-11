923d2816a09b5e34f4299729bf8f0c02061084a5
ethqnol <112029223+ethqnol@users.noreply.github.com>
2024-01-10 20:47:30 -0500

    Added basic control (untested)

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,26 @@
 #include "main.h"
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
+//replace later when we figure out what ports map to what
+const int LEFT_MOTOR_PORT = 1; 
+const int RIGHT_MOTOR_PORT = 2; 
+
+pros::Motor left_motor(LEFT_MOTOR_PORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor right_motor(RIGHT_MOTOR_PORT, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
+
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
- */ void initialize() {
+ */
+
+ void initialize() {
 	pros::lcd::initialize();
 	pros::lcd::set_text(1, "Hello PROS User!");
 
-	pros::lcd::register_btn1_cb(on_center_button);
 }
 
 /**
@@ -72,21 +67,15 @@ void autonomous() {}
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
+
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
+  while (true) {
+    int left_y = controller.get_analog(ANALOG_LEFT_Y);
+    int right_y = controller.get_analog(ANALOG_RIGHT_Y);
 
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
+    left_motor.move(left_y);
+    right_motor.move(right_y);
 
-		left_mtr = left;
-		right_mtr = right;
-
-		pros::delay(20);
-	}
+    pros::delay(2);
+  }
 }
